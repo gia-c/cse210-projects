5d82ed6127fc41678e7721b4450692b71e6519fb
gia-c <gissecasas@gmail.com>
2026-09-04 22:23:02 -0500

    Finished Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        // Determine the letter grade
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign
+        string sign = "";
+
+        int lastDigit = percent % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // A grades cannot have a +
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        // F grades cannot have a + or -
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // Display the final grade
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // Determine whether the student passed
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
